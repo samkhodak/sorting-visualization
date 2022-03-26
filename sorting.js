96fcc52f32a40bba49bb8ba98beae36d97929847
samkhodak <samkhodak@gmail.com>
2022-03-25 18:29:41 -0700

    Completed working sleep method to slow processing

--- sorting.js
+++ sorting.js
@@ -5,24 +5,17 @@ const reset_button = document.getElementById("reset-btn");
 let to_sort = [];
 array_size = 60;
 
-for (let i = 0; i < array_size; ++i) {
-  to_sort.push(random_height(15, 450));
-}
+randomize_array();
 
-quicksort(to_sort, 0, array_size - 1);
-console.log(to_sort);
-
-
-// console.log(to_sort);
-// swap_one_range(to_sort, 0, array_size - 1);
-
-// let sort = [35, 97, 99, 48, 12, 27, 103, 14, 32];
-// console.log(sort);
-// swap_one_range(sort, 0, 8);
 
 start_button.addEventListener("click", () => {
-  draw_frame(to_sort);
   start_button.disabled = true;
+  reset_button.disabled = true;
+  draw_frame(to_sort);
+  quicksort(to_sort, 0, array_size - 1);
+  console.log(to_sort);
+  start_button.disabled = false;
+  reset_button.disabled = false;
 });
 
 reset_button.addEventListener("click", () => {
@@ -31,20 +24,35 @@ reset_button.addEventListener("click", () => {
   start_button.disabled = false;
 });
 
-function quicksort(to_sort, min, max) {
-  if (min >= max)
-    return;
-  
-  let pivot = swap_one_range(to_sort, min, max);
+function randomize_array() {
+  //Clear array
+  to_sort = []; 
 
-  quicksort(to_sort, min, pivot - 1);
-  quicksort(to_sort, pivot + 1, max);
+  for (let i = 0; i < array_size; ++i) {
+   to_sort.push(random_height(15, 450));
 }
 
-function swap_one_range(to_sort, min, max) {
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function quicksort(to_sort, min, max) {
+  if (min >= max) return;
+
+  let pivot = await swap_one_range(to_sort, min, max);
+
+  await quicksort(to_sort, min, pivot - 1);
+  await quicksort(to_sort, pivot + 1, max);
+}
+
+async function swap_one_range(to_sort, min, max) {
   pivot = min;
   left = min + 1;
   right = max;
+
+  await sleep(250);
 
   do {
     while (to_sort[right] >= to_sort[pivot] && right != left) --right;
@@ -56,28 +64,38 @@ function swap_one_range(to_sort, min, max) {
       let a = to_sort[left];
       to_sort[left] = to_sort[right];
       to_sort[right] = a;
+      let bars = document.getElementsByClassName("array-bar");
+      clear_frame(bars);
+
+      draw_frame(to_sort);
     }
-  }
-  while(left != right)
+  } while (left != right);
 
   if (to_sort[pivot] > to_sort[right]) {
     let a = to_sort[pivot];
     to_sort[pivot] = to_sort[right];
     to_sort[right] = a;
 
-    return right;
-  }
-  else
-    return pivot;
+    let bars = document.getElementsByClassName("array-bar");
+    clear_frame(bars);
 
+    draw_frame(to_sort);
+
+    return right;
+  } else return pivot;
 }
 
+// function swap(to_sort, element_one, element_two) {}
+
 function draw_frame(to_sort) {
+  let i = 0;
   to_sort.forEach((element) => {
     let one_bar = document.createElement("div");
     one_bar.setAttribute("class", "array-bar");
+    one_bar.setAttribute("id", `"${element}"`);
     one_bar.style.height = `${element}px`;
     container.append(one_bar);
+    ++i;
   });
 }
 
